679c2405269f46e72bade7793ee10deebe2fd9b0
tfrasca <tyfrasc15@gmail.com>
2015-12-02 14:55:37 -0500

    changed pin location on for the constructor call, added configurePowerMode call, and wrote to CTRL_REG1 to setup the data rate and enable x,y,z axes

--- arduino/libraries/examples/LIS3DH_core_spi_test/LIS3DH_core_spi_test.ino
+++ arduino/libraries/examples/LIS3DH_core_spi_test/LIS3DH_core_spi_test.ino
@@ -10,10 +10,13 @@
 
 //configure the accelerometer chip
 
-LIS3DH_CoreSPIClass acc(6,   //slaveSelectLowPin
-                        15    //dataReadyLowPin
+//LIS3DH_CoreSPIClass acc(6,   //slaveSelectLowPin
+//                        15    //dataReadyLowPin
+//                       );
+LIS3DH_CoreSPIClass acc(10,   //slaveSelectLowPin
+                        6   //dataReadyLowPin
                        );
-
+int k;
 void setup() {
   Serial.begin(9600);
   //start up the SPI bus
@@ -25,6 +28,9 @@ void setup() {
   //SPI.setClockDivider(SPI_CLOCK_DIV21);  //FIXME SPI_CLOCK_DIV these names are not defined anymore in Arduino 1.5.4
   //start controlling the voltage supply
   acc.begin();
+  acc.configurePowerMode(2);
+  //need to configure data rate & enable x,y,z axes
+  acc._writeRegister(32,0b01110111); // FIXME this is only for powerMode 2, HIGH power. bits2:0 enable x,y,z axes. Bit3 will need to be changed.
   delay(1000);
   
 
@@ -32,14 +38,28 @@ void setup() {
 
 void loop() {
   delay(1000);
-  byte data;
-  for (int i = 0; i < LIS3DH::ADDR_MAX; i++){
-    data = acc._readRegister(i);
-    Serial.print(i);
-    Serial.print(": ");
-    print_binary(data, 8);
-    Serial.print("\n");
-  }
+  byte data,who_am_i;
+  int x_axis,y_axis,z_axis;
+  who_am_i = acc._readRegister(LIS3DH::WHO_AM_I);
+  //Serial.println(who_am_i);
+  x_axis = acc._readAcceleration(LIS3DH::OUT_X_L);
+  y_axis = acc._readAcceleration(LIS3DH::OUT_Y_L);
+  z_axis = acc._readAcceleration(LIS3DH::OUT_Z_L);
+  Serial.print("X: ");
+  Serial.print(x_axis);
+  Serial.print("     Y: ");
+  Serial.print(y_axis);
+  Serial.print("     Z: ");
+  Serial.println(z_axis);
+//  for (int i = 0; i < LIS3DH::ADDR_MAX; i++){
+//    data = acc._readRegister(i);
+//    Serial.print(i);
+//    Serial.print(": ");
+//    Serial.print(data);
+//    Serial.print(": ");
+//    print_binary(data, 8);
+//    Serial.print("\n");
+//  }
 }
 
 void print_binary(int v, int num_places)
